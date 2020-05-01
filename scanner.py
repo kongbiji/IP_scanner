0169de2ea530210ff5326449aa3633723586b63c
biji <heapoverflow@kakao.com>
2020-05-02 01:34:58 +0900

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,13 +4,13 @@ import sys
 s=[]
 
 for i in range(1,255):
-        ##서브넷 주소만 입력받기
+        ##input subnet
 	arp_req=Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=sys.argv[1]+"."+str(i))
 	arp_ans=srp1(arp_req, timeout=0.5)
 	if arp_ans:
-                ##존재한다면 저장
+                ## if ans exist, save
 		s.append(str(sys.argv[1]+"."+str(i)+" is at "+arp_ans[ARP].hwsrc))
 
-##실행중인 호스트들 출력
+## print hosts
 for j in range(0,len(s)):
 	print(s[j])
